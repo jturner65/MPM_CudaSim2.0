31c42cc129bb5c704db8836c3d565e2ce790b9dc
john turner <7strbass@gmail.com>
2020-02-12 17:15:17 -0500

    --smaller lib jars due to not bundling other libs in them.  DIRECTORY STRUCTURE MUST BE PRESERVED, or the jars won't find their deps!

--- src/MPM_CudaSim/MPM_SimWindow.java
+++ src/MPM_CudaSim/MPM_SimWindow.java
@@ -7,8 +7,8 @@ import base_UI_Objects.drawnObjs.myDrawnSmplTraj;
 import base_UI_Objects.windowUI.base.base_UpdateFromUIData;
 import base_UI_Objects.windowUI.base.myDispWindow;
 import base_UI_Objects.windowUI.myGUIObj;
-import base_Utils_Objects.vectorObjs.myPoint;
-import base_Utils_Objects.vectorObjs.myVector;
+import base_Math_Objects.vectorObjs.doubles.myPoint;
+import base_Math_Objects.vectorObjs.doubles.myVector;
 
 public class MPM_SimWindow extends myDispWindow {
 
