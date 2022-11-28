f4b0fb5d4e4b99f8d426f4061a15c8f0760dea9d
John Turner <7strbass@gmail.com>
2022-11-28 10:47:47 -0500

    --update libs;

--- src/MPM_CudaSim/ui/MPM_SimWindow.java
+++ src/MPM_CudaSim/ui/MPM_SimWindow.java
@@ -14,7 +14,7 @@ import base_Math_Objects.vectorObjs.doubles.myPoint;
 import base_Math_Objects.vectorObjs.doubles.myVector;
 import base_UI_Objects.GUI_AppManager;
 import base_UI_Objects.windowUI.base.Base_DispWindow;
-import base_UI_Objects.windowUI.drawnObjs.DrawnSimpleTraj;
+import base_UI_Objects.windowUI.drawnTrajectories.DrawnSimpleTraj;
 import base_UI_Objects.windowUI.uiData.UIDataUpdater;
 import base_UI_Objects.windowUI.uiObjs.base.GUIObj_Type;
 import base_Utils_Objects.io.messaging.MsgCodes;
